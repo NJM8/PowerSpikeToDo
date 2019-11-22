7acf5286dd16d3fcc349f8f4b5a640f25d765f9f
Nate Mallison <healthynate@gmail.com>
2019-11-22 10:00:56 -0500

    Updated title and favicon. Removed default app styling and components.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" class="bg-blue-400" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <style src="./assets/styles.css"></style>s
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+  name: "app"
 };
 </script>
 
@@ -23,8 +15,5 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
