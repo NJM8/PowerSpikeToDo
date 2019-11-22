106aa8f886f1ff4b113b80e5fe132356129e61b5
Nate Mallison <healthynate@gmail.com>
2019-11-22 10:31:25 -0500

    Created basic Vuex store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,25 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { vuexMutationHelpers } from "@/helpers/vuexMutationHelpers";
+import { vuexActionHelpers } from "@/helpers/vuexActionHelpers";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    todos: []
+  },
+  getters: {
+    todos: state => state.todos
+  },
+  mutations: {
+    SET_TODOS: vuexMutationHelpers.SET("todos"),
+    ADD_TODO: vuexMutationHelpers.ADD_TO_ARRAY("todos"),
+    DELETE_TODO: vuexMutationHelpers.DELETE_FROM_ARRAY("todos")
+  },
+  actions: {
+    getTodos: vuexActionHelpers("getTodos", "SET_TODOS"),
+    createTodo: vuexActionHelpers("createTodo", "ADD_TODO"),
+    deleteTodo: vuexActionHelpers("deleteTodo", "DELETE_TODO")
+  }
 });
