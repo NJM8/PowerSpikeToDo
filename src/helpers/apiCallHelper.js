203e4581374dfcb0ddfd4c10491a76323e646d85
Nate Mallison <healthynate@gmail.com>
2019-11-22 10:18:31 -0500

    Added api call helpers.

---
+++ src/helpers/apiCallHelper.js
@@ -0,0 +1,40 @@
+import todoApi from "@/api/ApiConfiguration";
+
+export const apiCallHelper = {
+  GET: (url, params) => {
+    return new Promise((resolve, reject) => {
+      todoApi
+        .get(url, params)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  CREATE: (url, params) => {
+    return new Promise((resolve, reject) => {
+      todoApi
+        .post(url, params)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  DELETE: url => {
+    return new Promise((resolve, reject) => {
+      todoApi
+        .delete(url)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+};
