4e5f48b3b6977e692ba7e960489c19626252683b
Nate Mallison <healthynate@gmail.com>
2019-11-25 13:51:15 -0500

    Configuration for cxlt-toaster

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import CxltToastr from "cxlt-vue2-toastr";
+
+const toastrConfigs = {
+  position: "bottom left",
+  timeOut: 4000,
+  showMethod: "slideInLeft",
+  hideMethod: "slideOutLeft",
+  showDuration: 400,
+  hideDuration: 400
+};
+
+Vue.use(CxltToastr, toastrConfigs);
 
 Vue.config.productionTip = false;
 
