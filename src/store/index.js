5869a4766dc71b3a1708c65eb47b1fc4cd06fb94
Nate Mallison <healthynate@gmail.com>
2019-11-25 13:54:36 -0500

    Updated get action to send a toaster message. Updated create action to re-fetch todos.

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import { vuexMutationHelpers } from "@/helpers/vuexMutationHelpers";
 import { vuexActionHelpers } from "@/helpers/vuexActionHelpers";
+import todoApi from "@/api/todoApi";
 
 Vue.use(Vuex);
 
@@ -18,8 +19,33 @@ export default new Vuex.Store({
     DELETE_TODO: vuexMutationHelpers.DELETE_FROM_ARRAY("todos")
   },
   actions: {
-    getTodos: vuexActionHelpers.CRU("getTodos", "SET_TODOS"),
-    createTodo: vuexActionHelpers.CRU("createTodo", "ADD_TODO"),
+    getTodos: ({ commit }) => {
+      todoApi
+        .getTodos()
+        .then(response => {
+          commit("SET_TODOS", response);
+        })
+        .catch(() => {
+          Vue.prototype.$toast.error({
+            title: "Error",
+            message:
+              "There was a problem fetching your todos, please refresh the page."
+          });
+        });
+    },
+    createTodo: ({ dispatch }, params) => {
+      return new Promise((resolve, reject) => {
+        todoApi
+          .createTodo(params)
+          .then(() => {
+            dispatch("getTodos");
+            resolve();
+          })
+          .catch(error => {
+            reject(error);
+          });
+      });
+    },
     deleteTodo: vuexActionHelpers.DELETE("deleteTodo", "DELETE_TODO")
   }
 });
